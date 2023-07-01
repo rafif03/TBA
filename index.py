a3d753a78a3a257f0a5bd913ce554df25ef37439
Rafif <naufal.rafif@student.uns.ac.id>
2023-07-01 22:43:37 +0700

    modified:   index.py modified:   sqr.py

--- index.py
+++ index.py
@@ -20,34 +20,42 @@ while True:
     user_input = int(input("Enter an integer: "))
     if user_input == 1:
         print("Penjumlahan Selected")
+        print("input: 0+0, 0+1, 1+1, 1+0")
         add()
         break
     elif user_input == 2:
         print("Pembagian Selected")
+        print("input: 0/0, 0/1, 1/1, 1/0")
         div()
         break
     elif user_input == 3:
         print("Factorial Selected")
+        print("input: 0!")
         fac()
         break
     elif user_input == 4:
         print("Logaritma Selected")
+        print("input: 0")
         log2()
         break
     elif user_input == 5:
         print("Perkalian Selected")
+        print("input: 0*0, 0*1, 1*0, 1*1")
         mul()
         break
     elif user_input == 6:
         print("Pangkat Selected")
+        print("input: 0^0")
         power()
         break
     elif user_input == 7:
         print("Pengurangan Selected")
+        print("input: 0-0, 0-1, 1-1, 1-0")
         sub()
         break
     elif user_input == 8:
         print("Akar Kuadrat Selected")
+        print("input: ")
         sqr()
         break
     print("This number is even")
--- sqr.py
+++ sqr.py
@@ -8,45 +8,43 @@ def sqr():
     masuk2 = "B" * len(masuk1)
     masuk3 = masuk2
     masuk4 = masuk2
-    masuk5 = masuk2
-    index1, index2, index3, index4, index5 = 100, 100, 100, 100, 100
+    index1, index2, index3, index4 = 100, 100, 100, 100
     tape1 = list(masuk1)
     tape2 = list(masuk2)
     tape3 = list(masuk3)
     tape4 = list(masuk4)
-    tape5 = list(masuk5)
     state = "q0"
     print(tape1[index1-30:index1 + 5])
     print(tape2[index2-30:index2 + 5])
     print(tape3[index3-30:index3 + 5])
     print(tape4[index4-30:index4 + 5])
-    print(tape5[index5-30:index5 + 5])
     print("state : " + state)
     dict1 = {
-        "q0" : [["1BBBB", "1BBB1", "SSSSS", "q1"]],
-        "q1" : [["1BBB1", "11BB1", "SRSSL", "q1"], ["1BBBB", "1CBBB", "SRSSR", "q2"]],
-        "q2" : [["1BBB1", "11BB1", "SRSSR", "q2"], ["1BBBB", "1CBBB", "SLSSS", "q3"]],
-        "q3" : [["11BBB", "111BB", "SLRSS", "q3"], ["1BBBB", "1CBBB", "SLLSS", "q4"]],
-        "q4" : [["111BB", "1111B", "SSLRS", "q4"], ["11BBB", "11BBB", "SRLSS", "q5"], ["1B1BB", "1B1BB", "SRSLS", "q6"]],
-        "q5" : [["111BB", "1111B", "SSRRS", "q5"], ["11BBB", "11BBB", "SLLSS", "q4"], ["1B1BB", "1B1BB", "SRSLS", "q6"]],
-        "q6" : [["1111B", "1111B", "SSLSS", "q6"], ["11B1B", "11B1B", "SSRSS", "q7"]],
-        "q7" : [["1111B", "110BB", "RSSLS", "q7"], ["111BB", "111BB", "LSSLS", "q8"], ["B111B", "B1111", "SSSSL", "q10"], ["B11BB", "B11BB", "SSSSS", "q11"]],
-        "q8" : [["11BBB", "100BB", "LRRLS", "q8"], ["11BBB", "1C0BB", "LRRLS", "q8"], ["01BBB", "00BBB", "RRSRS", "q9"]],
-        "q9" : [["111BB", "000BB", "SRRSR", "q9"], ["1BBBB", "1BBB1", "SSSSS", "q0"]],
-        "q10" : [["B1111", "B1110", "SSSSS", "q10"]],
-        "q11" : [[]]
+        "q0" : [["1BBB", "1111", "RSSR", "q1"]],
+        "q1" : [["111B", "111B", "RSSS", "q2"], ["B11B", "B11B", "SRRS", "q4"]],
+        "q2" : [["B11B", "B11B", "SRRS", "q4"], ["111B", "111B", "RSSS", "q3"]],
+        "q3" : [["B11B", "B11B", "SRRS", "q3"], ["111B", "111B", "SSSS", "q5"]],
+        "q4" : [[]],
+        "q5" : [["111B", "111B", "LLLS", "q5"], ["111B", "111B", "LSSS", "q5"], ["BBBB", "BBBB", "RRRS", "q6"]],
+        "q6" : [["111B", "111B", "RSRS", "q6"], ["11BB", "11BB", "SRLS", "q7"], ["1BBB", "1BBB", "SSSS", "q8"], ["1B1B", "1B1B", "SSRS", "q8"], ["BBBB", "BBBB", "SSSS", "q9"], ["B11B", "B11B", "SSSS", "q9"], ["B1BB", "B1BB", "SSSS", "q9"], ["BB1B", "BB1B", "SSSS", "q9"]],
+        "q7" : [["111B", "111B", "RSLS", "q7"], ["11BB", "11BB", "SRRS", "q6"], ["1BBB", "1BBB", "SSSS", "q8"], ["1B1B", "1B1B", "SSRS", "q8"], ["BBBB", "BBBB", "SSSS", "q10"], ["B11B", "B11B", "SSSS", "q10"], ["B1BB", "B1BB", "SSSS", "q10"], ["BB1B", "BB1B", "SSSS", "q10"]],
+        "q8" : [["1B1B", "1B1B", "SSRS", "q8"], ["1BBB", "1111", "SSSR", "q5"]],
+        "q9" : [["BBBB", "BBBB", "SSSS", "q11"], ["B11B", "B11B", "SRRL", "q11"], ["B1BB", "B1BB", "SRSL", "q11"], ["BB1B", "BB1B", "SSRL", "q11"]],
+        "q10" : [["BBBB", "BBBB", "SSSS", "q12"], ["B11B", "B11B", "SRRL", "q12"], ["B1BB", "B1BB", "SRSL", "q12"], ["BB1B", "BB1B", "SSRL", "q12"]],
+        "q11" : [["BBBB", "BBBB", "SSSS", "q13"], ["BBB1", "BBB1", "SSSS", "q13"], ["B111", "B11B", "SSSS", "q13"], ["B1B1", "B1BB", "SSSS", "q13"], ["BB11", "BB1B", "SSSS", "q13"]],
+        "q12" : [["BBBB", "BBBB", "SSSS", "q13"], ["BBB1", "BBB1", "SSSS", "q13"], ["B111", "B11B", "SSSS", "q13"], ["B1B1", "B1BB", "SSSS", "q13"], ["BB11", "BB1B", "SSSS", "q13"]],
+        "q13" : [[]]
         }
 
     while (1):
         i = 0
         while (i < len(dict1[state])):
             #print(i)
-            if(tape1[index1] + tape2[index2] + tape3[index3] + tape4[index4] + tape5[index5] == dict1[state][i][0]):
+            if(tape1[index1] + tape2[index2] + tape3[index3] + tape4[index4] == dict1[state][i][0]):
                 tape1[index1] = dict1[state][i][1][0]
                 tape2[index2] = dict1[state][i][1][1]
                 tape3[index3] = dict1[state][i][1][2]
                 tape4[index4] = dict1[state][i][1][3]
-                tape5[index5] = dict1[state][i][1][4]
                 if(dict1[state][i][2][0] == "R"):
                     index1 += 1
                 elif(dict1[state][i][2][0] == "L"):
@@ -63,20 +61,18 @@ def sqr():
                     index4 += 1
                 elif(dict1[state][i][2][3] == "L"):
                     index4 -= 1
-                if(dict1[state][i][2][4] == "R"):
-                    index5 += 1
-                elif(dict1[state][i][2][4] == "L"):
-                    index5 -= 1
                 state = dict1[state][i][3]
                 print(tape1[index1-30:index1 + 5])
                 print(tape2[index2-30:index2 + 5]) 
                 print(tape3[index3-30:index3 + 5])
                 print(tape4[index4-30:index4 + 5])
-                print(tape5[index5-30:index5 + 5])
                 print("state : " + state)
-                if state == "q11":
+                if state == "q13":
                     exit("Diterima, hitung jumlah 0 di tape 4")
-                break  
+                    break
+                elif state == "q4":
+                    exit("Diterima, hitung jumlah 0 di tape 4")
+                break
             i += 1 
             if i >= len(dict1[state]):
                 exit("Tolak")
